baa2449ff8b96372f1fc123120b4518646b28f9c
shj20000107 <84953517+shj20000107@users.noreply.github.com>
2023-03-08 18:18:31 +0800

    Add files via upload

---
+++ FlowerClassification/app.js
@@ -0,0 +1,19 @@
+// app.js---小程序项目的入口文件
+App({// 通过调用App()函数来启动整个小程序
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ FlowerClassification/utils/util.js
@@ -0,0 +1,22 @@
+// 用于存放工具性质的模块
+
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : `0${n}`
+}
+
+//向外共享该模块
+module.exports = {
+  formatTime
+}
